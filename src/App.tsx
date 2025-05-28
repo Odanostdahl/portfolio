aa8f04f07db1f8284a9ade5468dd5660373fa883
odanostdahl <oda.nostdahl@gmail.com>
2025-05-28 10:57:03 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Oda Nøstdahl</h1>
+
+      <div className="about-me">
+        <p>
+            Hi, I´m Oda and I´m currently in my second year of a Bachelor's degree in programming at Kristiania.
+
+            Beyond academics, I’m passionate about creating a more inclusive tech environment. I actively contribute to this through my role as co-leader on the board of Kvinner&IT💓
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
